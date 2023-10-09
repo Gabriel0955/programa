5ea6caf4d60300b103fc48751ce9959cdba19a5d
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2023-10-09 15:46:19 -0500

    Update settings.py

--- Sistema_de_ventas_ropa/settings.py
+++ Sistema_de_ventas_ropa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-p9tkaidc20a1qn2$wddclmjqq=h8=7ognl_c#@6#m%1&p$%ext
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tiendaropa.onrender.com']
+ALLOWED_HOSTS = ['tiendaropa-5sm0.onrender.com']
 
 
 # Application definition
